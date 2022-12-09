4b3789178f9aa3f4973b8633163bf655f91f6c4e
Jingyan Zhang <105741006+jingaling00@users.noreply.github.com>
2022-12-09 17:18:29 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -134,6 +134,9 @@ class CarbonFiller:
         for row in pg:
             row *= np.around(math.sqrt(3)/2,decimals=3)
         for row in qg:
+            for i in range(1,len(row)):
+                if row[i] != row[i-1]:
+                    return None
             if row[0] % 2 == 0:
                 for i in range(0,qg.shape[1],2):
                     row[i] *= 1.5
